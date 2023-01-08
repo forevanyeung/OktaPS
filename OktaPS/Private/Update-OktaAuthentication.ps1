a533a8cc3daa8fabb2e5770033b8dcee89c7a848
Evan Yeung <evan.yeung@clearme.com>
2023-01-08 18:15:35 -0500

    fix: expired Okta authentication

--- OktaPS/Private/Update-OktaAuthentication.ps1
+++ OktaPS/Private/Update-OktaAuthentication.ps1
@@ -19,7 +19,7 @@ Function Update-OktaAuthentication {
 
             Write-Host "Okta session expired ($Script:OktaSSOExpirationUTC UTC)"
             # Remove-Variable OktaSSO,OktaSSOExpirationUTC -Scope Script
-            Connect-Okta -OrgUrl $Script:OktaOrg -Credential $Script:OktaUsername
+            Connect-Okta -OrgUrl $Script:OktaDomain -Credential $Script:OktaUsername
         }
 
         "SSWS" {
