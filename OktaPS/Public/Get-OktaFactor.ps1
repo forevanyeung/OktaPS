61d075507c82b447339783d23eab6f965545f319
Evan Yeung <evan.yeung@clearme.com>
2023-02-07 09:26:56 -0500

    fix: lookup by oktauser

--- OktaPS/Public/Get-OktaFactor.ps1
+++ OktaPS/Public/Get-OktaFactor.ps1
@@ -11,10 +11,12 @@ Function Get-OktaFactor {
     }
 
     Process {
-        Foreach($u in $OktaUser) {
+        Foreach($u in $Identity) {
+            Write-Verbose "Looking up factor for $($u.login)"
+
             # count is not available when pipelining so we cannot show a progress bar, alternative to
             # show a count of processed objects
-            $ObjectPct = $OktaUser.Count -gt 1 ? $i/($OktaUser.Count)*100 : -1
+            $ObjectPct = $Identity.Count -gt 1 ? $i/($Identity.Count)*100 : -1
 
             Write-Progress -Activity "Getting enrolled factors" -Status $i -PercentComplete $ObjectPct
             Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/users/$($u.Id)/factors"
