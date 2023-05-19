430e12ff8738314dd75eb29bdeb021d56ae541cb
Evan Yeung <evan.yeung@clearme.com>
2023-05-19 00:39:54 -0400

    feat: filter Okta group type

--- OktaPS/Public/Get-OktaGroup.ps1
+++ OktaPS/Public/Get-OktaGroup.ps1
@@ -1,10 +1,15 @@
 Function Get-OktaGroup {
     [CmdletBinding()]
     param (
+        # Specifies an ID or name of an Okta group to retrieve. If searching by ID, it must be an exact match and only 
+        # one result will be returned. If searching by name, it will conduct a starts with query, and if multiple matches
+        # all groups will be returned.
         [Parameter(Mandatory=$true)]
         [String]
         $Name,
 
+        # Specifies a specific type of group to search for. If no Type is specified, the default is to search for all 
+        # types. Does not apply when searching by ID.
         [Parameter()]
         [ValidateSet("OKTA_GROUP","APP_GROUP")]
         [String]
@@ -14,8 +19,15 @@ Function Get-OktaGroup {
     # try matching group id
     $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups/$Name" -ErrorAction SilentlyContinue
     If(-not $group) {
+        $query = @{}
+        $query["q"] = $Name
+
+        If($Type) {
+            $query["filter"] = "type eq `"$Type`""
+        } 
+
         # try matching group name
-        $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups?q=$Name" -ErrorAction SilentlyContinue
+        $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups" -Query $query -ErrorAction SilentlyContinue
     }
 
     If(-not $group) {
@@ -25,7 +37,6 @@ Function Get-OktaGroup {
     $GroupObject = Foreach($g in $group) {
         ConvertTo-OktaGroup -InputObject $g
     }
-    Return $GroupObject
 
-    # Return $group
+    Return $GroupObject
 }
