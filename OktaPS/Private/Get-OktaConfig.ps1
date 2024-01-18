79257bbd53ecb4e9139daa523610df1cfbfc0de5
Evan Yeung <evan.yeung@clearme.com>
2024-01-18 15:32:30 -0500

    feat: add argument completion to Get-OktaConfig and simple matching

--- OktaPS/Private/Get-OktaConfig.ps1
+++ OktaPS/Private/Get-OktaConfig.ps1
@@ -2,6 +2,7 @@ Function Get-OktaConfig {
     [CmdletBinding()]
     param (
         [Parameter()]
+        [ArgumentCompleter({ OktaConfigPathArgumentCompleter @args })]
         [String]
         $Path
     )
@@ -17,40 +18,46 @@ Function Get-OktaConfig {
             Write-Error "Could not find Okta config file at $Path"
             Return
         }
-
-        # Search for the file in the .okta directory of the current working directory
-        If(Test-Path ($configPath = Join-Path ".okta" $Path)) {
-            Return $configPath
-        }
-
-        # Search for the file in the .okta directory of the user's home directory
-        If(Test-Path ($configPath = Join-Path $($env:HOME ?? $env:USERPROFILE) ".okta" $Path)) {
-            Return $configPath
-        }
-
-        Write-Error "Could not find Okta config file at $Path"
-        Return
     }
 
-    # If a path is provided, search in one of the following locations in order of precedence:
+    $config = OktaConfigPathArgumentCompleter -wordToComplete $Path
+    
+    If($config.Count -eq 0) {
+        Write-Error "Could not find Okta config file"
+        Return
+    }
+    
+    Return $config[0].FullName
+}
+
+Function OktaConfigPathArgumentCompleter {
+    [CmdletBinding()]
+    param (
+        $commandName,
+        $parameterName,
+        $wordToComplete,
+        $commandAst,
+        $fakeBoundParameters
+    )
+
+    # Search in one of the following locations in order of precedence:
     # https://developer.okta.com/docs/guides/implement-oauth-for-okta-serviceapp/main/
     # 1. Environment variables (in this case, cmdlet parameters)
     # 2. An okta.yaml file in a .okta folder in the application or project's root directory
     # 3. An okta.yaml file in a .okta folder in the current user's home directory (~/.okta/okta.yaml or %userprofile%\.okta\okta.yaml)
-    
-    If(Test-Path ($configPath = Join-Path ".okta" "okta.yaml")) {
-        Return $configPath
+
+    $location = @(
+        (Join-Path (Get-Location) ".okta"),
+        (Join-Path $($env:HOME ?? $env:USERPROFILE) ".okta")
+    )
+
+    $configs = Foreach($path in $location) {
+        Get-ChildItem -Path $path -Filter "okta.yaml" -Recurse
+        Get-ChildItem -Path $path -Filter "okta.yml" -Recurse
+        Get-ChildItem -Path $path -Filter "*.yaml" -Recurse | Where-Object { $_.name -notlike "okta.yaml" }
+        Get-ChildItem -Path $path -Filter "*.yml" -Recurse | Where-Object { $_.name -notlike "okta.yml" }
     }
 
-    If(Test-Path ($configPath = Join-Path ".okta" "okta.yml")) {
-        Return $configPath
-    }
-
-    If(Test-Path ($configPath = Join-Path $($env:HOME ?? $env:USERPROFILE) ".okta" "okta.yaml")) {
-        Return $configPath   
-    }
-
-    If(Test-Path ($configPath = Join-Path $($env:HOME ?? $env:USERPROFILE) ".okta" "okta.yml")) {
-        Return $configPath   
-    }
+    $configs | Where-Object { $_.name -like "*$wordToComplete*" }
 }
+
