c149e28feb9367e53f679c0ad0aedad2538fb65d
Evan Yeung <evan@forevanyeung.com>
2024-04-07 12:57:11 -0400

    wip: parallelize reqeusts

--- OktaPS/Private/Invoke-OktaRequest.ps1
+++ OktaPS/Private/Invoke-OktaRequest.ps1
@@ -2,7 +2,7 @@ Function Invoke-OktaRequest {
     [CmdletBinding(SupportsShouldProcess)]
     param (
         [Parameter()]
-        [String]
+        [Microsoft.PowerShell.Commands.WebRequestMethod]
         $Method = "GET",
 
         [Parameter(Mandatory)]
@@ -27,7 +27,11 @@ Function Invoke-OktaRequest {
 
         [Parameter()]
         [Switch]
-        $NoPagination
+        $NoPagination,
+
+        [Parameter()]
+        [Switch]
+        $NoAsync
     )
 
     $webrequest_parameters = @{}
@@ -80,45 +84,84 @@ Function Invoke-OktaRequest {
     if ($PSCmdlet.ShouldProcess($request_uri)) {
         # supports pagination
         $next = $True
-        $return = while($next) {
-            $Script:OktaDebugLastRequestUri = $request_uri
-            try {
-                $response = Invoke-WebRequest -Method $Method -Uri $request_uri -Headers $built_headers -SkipHeaderValidation @webrequest_parameters
-            } catch [Microsoft.PowerShell.Commands.HttpResponseException] {
-                If($_.Exception.Response.StatusCode -eq 429) {
-                    Write-Debug "X-Rate-Limit-Limit: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Limit'))"
-                    Write-Debug "X-Rate-Limit-Remaining: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Remaining'))"
-                    Write-Debug "X-Rate-Limit-Reset: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Reset'))"
 
-                    $limit_reset = [System.DateTimeOffset]::FromUnixTimeSeconds($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Reset')[0])
-                    $offset = $limit_reset.Offset.TotalSeconds
-                    Write-Host "Okta Rate Limit Exceeded. $($limit_reset.LocalDateTime.ToString())"
+        if($Method -eq "GET" -and $NoAsync -eq $False) {
+            Write-Verbose "Collecting pagination links"
 
-                    # TODO: wait until time elapses and continue
-                    Start-Sleep -Seconds $offset
-                }
-            } catch {
-                Write-Host "Unknown error occurred."
-                Throw $_
-            }
+            $paginationCollector = while($next) {
+                $response = Invoke-WebRequest -Method "HEAD" -Uri $request_uri -Headers $built_headers -SkipHeaderValidation @webrequest_parameters
 
-            # Response
-            If($PassThru) {
-                $response
-
-            } elseif(($response.StatusCode -ge 200) -and ($response.StatusCode -le 299)) {
-                $response.Content | ConvertFrom-Json
+                # sent url to collector
+                $request_uri
                 
-            } else {
-                # uncaught status code, return the raw and exit
-                Return $response
+                # pagination
+                If($response.RelationLink.ContainsKey('next') -and ($NoPagination -eq $False)) {
+                    $request_uri = $response.RelationLink['next']
+                } else {
+                    $next = $False
+                }
             }
 
-            # pagination
-            If($response.RelationLink.ContainsKey('next') -and ($NoPagination -eq $False)) {
-                $request_uri = $response.RelationLink['next']
-            } else {
-                $next = $False
+            Write-Verbose "Collected $($paginationCollector.Count) pages for pagination"
+
+            $return = $paginationCollector | ForEach-Object -ThrottleLimit 5 -Parallel {
+                $response = Invoke-WebRequest -Method $using:Method -Uri $_ -Headers $using:built_headers -SkipHeaderValidation @using:webrequest_parameters
+
+                If($using:PassThru) {
+                    $response
+                } elseif(($response.StatusCode -ge 200) -and ($response.StatusCode -le 299)) {
+                    $response.Content | ConvertFrom-Json
+                } else {
+                    # uncaught status code, return the raw and exit
+                    Return $response
+                }
+            }
+
+            Return $return
+
+        } else {
+            $return = while($next) {
+                $Script:OktaDebugLastRequestUri = $request_uri
+                try {
+                    $response = Invoke-WebRequest -Method $Method -Uri $request_uri -Headers $built_headers -SkipHeaderValidation @webrequest_parameters
+
+                    Write-Verbose $request_uri
+                } catch [Microsoft.PowerShell.Commands.HttpResponseException] {
+                    If($_.Exception.Response.StatusCode -eq 429) {
+                        Write-Debug "X-Rate-Limit-Limit: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Limit'))"
+                        Write-Debug "X-Rate-Limit-Remaining: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Remaining'))"
+                        Write-Debug "X-Rate-Limit-Reset: $($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Reset'))"
+
+                        $limit_reset = [System.DateTimeOffset]::FromUnixTimeSeconds($_.Exception.Response.Headers.GetValues('X-Rate-Limit-Reset')[0])
+                        $offset = $limit_reset.Offset.TotalSeconds
+                        Write-Host "Okta Rate Limit Exceeded. $($limit_reset.LocalDateTime.ToString())"
+
+                        # TODO: wait until time elapses and continue
+                        Start-Sleep -Seconds $offset
+                    }
+                } catch {
+                    Write-Host "Unknown error occurred."
+                    Throw $_
+                }
+
+                # Response
+                If($PassThru) {
+                    $response
+
+                } elseif(($response.StatusCode -ge 200) -and ($response.StatusCode -le 299)) {
+                    $response.Content | ConvertFrom-Json
+                    
+                } else {
+                    # uncaught status code, return the raw and exit
+                    Return $response
+                }
+
+                # pagination
+                If($response.RelationLink.ContainsKey('next') -and ($NoPagination -eq $False)) {
+                    $request_uri = $response.RelationLink['next']
+                } else {
+                    $next = $False
+                }
             }
         }
     }
