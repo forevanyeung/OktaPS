43b258a66ac2486d611afacbba3712ea94d17a3a
Evan Yeung <evan@forevanyeung.com>
2024-05-01 15:49:37 -0400

    feat: return list of all groups when no name is specified

--- OktaPS/Public/Get-OktaGroup.ps1
+++ OktaPS/Public/Get-OktaGroup.ps1
@@ -1,10 +1,10 @@
 Function Get-OktaGroup {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='AllGroups')]
     param (
         # Specifies an ID or name of an Okta group to retrieve. If searching by ID, it must be an exact match and only 
         # one result will be returned. If searching by name, it will conduct a starts with query, and if multiple matches
         # all groups will be returned.
-        [Parameter(Mandatory=$true)]
+        [Parameter(ParameterSetName="GetGroup", Mandatory=$true, Position=0)]
         [String]
         $Name,
 
@@ -16,18 +16,25 @@ Function Get-OktaGroup {
         $Type
     )
 
-    # try matching group id
-    $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups/$Name" -ErrorAction SilentlyContinue
-    If(-not $group) {
-        $query = @{}
-        $query["q"] = $Name
-
-        If($Type) {
-            $query["filter"] = "type eq `"$Type`""
-        } 
-
-        # try matching group name
-        $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups" -Query $query -ErrorAction SilentlyContinue
+    $query = @{}
+    If($Type) {
+        $query["filter"] = "type eq `"$Type`""
+    } 
+    
+    switch ($PsCmdlet.ParameterSetName) {
+        "AllGroups" {
+            $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups" -Query $query -ErrorAction SilentlyContinue
+        }
+        
+        "GetGroup" {
+            # try matching group id
+            $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups/$Name" -ErrorAction SilentlyContinue
+            If(-not $group) {
+                # try matching group name
+                $query["q"] = $Name
+                $group = Invoke-OktaRequest -Method "GET" -Endpoint "/api/v1/groups" -Query $query -ErrorAction SilentlyContinue
+            }
+        }
     }
 
     If(-not $group) {
