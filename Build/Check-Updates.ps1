d9b6d77dfeb1a61b803e225baa79cfa883795c7e
Evan Yeung <evan@forevanyeung.com>
2024-08-27 11:25:35 -0400

    feat: check only the PSGallery repository for new versions

--- Build/Check-Updates.ps1
+++ Build/Check-Updates.ps1
@@ -5,7 +5,11 @@ If($env:OKTAPS_CHECK_UPDATES -eq "false") {
 }
 
 $installedVersion = Get-Module -Name $ModuleName -ListAvailable | Select-Object -ExpandProperty Version
-$onlineVersion = Find-Module -Name $ModuleName | Select-Object -ExpandProperty Version
+
+# Get the default PSGallery repository
+$repository = Get-PSRepository | Where-Object { $_.SourceLocation -eq "https://www.powershellgallery.com/api/v2/" } | select -ExpandProperty Name
+
+$onlineVersion = Find-Module -Name $ModuleName -Repository $repository | Select-Object -ExpandProperty Version
 
 If($onlineVersion -gt $installedVersion) {
     Write-Host -ForegroundColor "Yellow" -Object "New version is available: $installedVersion -> $onlineVersion. Run 'Update-Module -Name $ModuleName' to update."
