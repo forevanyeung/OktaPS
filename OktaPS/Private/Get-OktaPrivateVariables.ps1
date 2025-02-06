5c8f3390d12e327c6761555043156dadd64ff297
Evan Yeung <evan@forevanyeung.com>
2025-02-06 13:16:22 -0500

    docs: wip update authorization code

--- OktaPS/Private/Get-OktaPrivateVariables.ps1
+++ OktaPS/Private/Get-OktaPrivateVariables.ps1
@@ -1,13 +1,13 @@
 # Example output:
 # Name                           Value
 # ----                           -----
-# OktaAdminDomain                https://clearme-admin.okta.com
+# OktaAdminDomain                https://dev-8675309-admin.okta.com
 # OktaAuthorizationMode          AuthorizationCode
-# OktaDomain                     https://clearme.okta.com
+# OktaDomain                     https://dev-8675309.okta.com
 # OktaOAuthClientId              0oal870p8yln0so89297
-# OktaOAuthRefreshToken          
+# OktaOAuthRefreshToken          System.Security.SecureString
 # OktaOAuthToken                 System.Security.SecureString
-# OktaOrg                        clearme
+# OktaOrg                        dev-8675309
 # OktaSSO                        Microsoft.PowerShell.Commands.WebRequestSession
 # OktaSSOExpirationUTC           2/1/2025 6:25:42 AM
 Function Get-OktaPrivateVariables {
